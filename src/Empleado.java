31bb8b11c0d9c1b8af29330e622f2d299d64b29b
MauricioMN <moryseme@gmail.com>
2022-12-05 00:08:52 +0100

    probando objeto empleado

---
+++ src/Empleado.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Empleado {
+
+    final String nombre;
+    Date birthDate;
+
+    Empleado(String name, Date birthDate) {
+        this.nombre = name;
+        this.birthDate = birthDate;
+    }
+}
